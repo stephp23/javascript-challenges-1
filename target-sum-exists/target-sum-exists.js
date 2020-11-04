774edb0e1ed9217158a01105567137f032ab7db3
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-11-04 00:03:12 -0500

    in class lab

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,8 +1,22 @@
 const targetSumExistsInTwoNumbers = (list, target) => {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] + list[j] === target) {
+        return true
+      }
+    }
+  }
   return false
 }
 
 const targetSumExistsInThreeNumbers = (list, target) => {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] + list[j] === target) {
+        return true
+      }
+    }
+  }
   return false
 }
 
